1d917a3b407f6af3e7f7f6564e8726f6ae550688
Mervan Akbulut <mmakbulut@gmail.com>
2025-07-23 16:18:12 +0300

    Refactor main function to improve variable initialization and pointer usage

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 
 int main(void)
 {
+    int a = 32;
+    int b = 32;
+    int c = 32;
+    int d = 32;
+    ft_printf("a:%p\nb:%p\nc:%p\nd:%p\n", &a, &b, &c, &d);
     ft_printf("Hello %s! Your number is %d and hex is %x.\n", "World", 42, 255);
-    ft_printf("Unsigned number: %u, Pointer: %p, Percent sign: %%\n", 12345, (void *)0x7ffeefbff5c0);
+    ft_printf("Unsigned number: %u, Pointer: %p, Percent sign: %%\n", 12345, &a);
     ft_printf("Character: %c, String: %s\n", 'A', "Hello, 42istanbul!");
     ft_printf("Hexadecimal (lowercase): %x, Hexadecimal (uppercase): %X\n", 255, 255);
     ft_printf("Negative number: %d\n", -42);
