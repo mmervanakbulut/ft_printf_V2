1730c307babeba218d29ab8bff6fb6a14cb9719c
Mustafa Mervan Akbulut <musakbul@k1m34s07.42istanbul.com.tr>
2025-07-24 19:42:26 +0300

    return (nil)

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: musakbul <musakbul@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 19:16:25 by musakbul          #+#    #+#             */
-/*   Updated: 2025/07/22 19:16:25 by musakbul         ###   ########.fr       */
+/*   Updated: 2025/07/24 19:41:37 by musakbul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	ft_print_pointer(void *ptr)
 	int	len;
 
 	if (!ptr)
-		return (write(1, "0x0", 3));
+		return (write(1, "(nil)", 5));
 	len = write(1, "0x", 2);
 	len += ft_putptr((unsigned long)ptr);
 	return (len);
--- main.c
+++ main.c
@@ -1,23 +1,25 @@
 #include "ft_printf.h"
-#include <stdio.h>
 
-int main(void)
+int funct(int a, ...)
 {
-    int a = 32;
-    int b = 32;
-    int c = 32;
-    int d = 32;
-    char *str = "adana";
-    int len = ft_printf("%s\n", str);
-    ft_printf("%d\n", len);
-    int len2 = printf("%s\n", str);
-    printf("%d\n", len2);
-    ft_printf("a:%p\nb:%p\nc:%p\nd:%p\n", &a, &b, &c, &d);
-    ft_printf("Hello %s! Your number is %d and hex is %x.\n", "World", 42, 255);
-    ft_printf("Unsigned number: %u, Pointer: %p, Percent sign: %%\n", 12345, &a);
-    ft_printf("Character: %c, String: %s\n", 'A', "Hello, 42istanbul!");
-    ft_printf("Hexadecimal (lowercase): %x, Hexadecimal (uppercase): %X\n", 255, 255);
-    ft_printf("Negative number: %d\n", -42);
+	va_list args;
+	int i = 0;
+	int total = 0;
 
-    return 0;
+	va_start(args, a);
+
+	while (i < a)
+	{
+		total += va_arg(args, int);
+		i++;
+	}
+	va_end(args);
+	return (total);
+}
+
+
+int main()
+{
+	int result = funct(5, 1,2,3,4,5);
+	ft_printf("%i", result);
 }
